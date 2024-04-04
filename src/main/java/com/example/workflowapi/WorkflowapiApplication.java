5c1e7514056591da51021985d28aaca0a7bfa910
Mateusz <mateusz.baranski8@gmail.com>
2024-04-04 11:52:29 +0200

    Init commit

---
+++ src/main/java/com/example/workflowapi/WorkflowapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.workflowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowapiApplication.class, args);
+	}
+
+}
