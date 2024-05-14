295cbd6157e42276c398e9ae51c529f578b754f2
Mateusz <mateusz.baranski8@gmail.com>
2024-05-14 11:08:14 +0200

    WF08 - Removed DTO classes and use entity instead of it. Moved part of try-catch blocks from controller to service layer.

--- src/test/java/com/example/workflowapi/controllers/CommentControllerTest.java
+++ src/test/java/com/example/workflowapi/controllers/CommentControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
